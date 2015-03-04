9484c08c4263bd420154e42e65b87b4ce9ef95b2
Matthias <mail@matthiasbock.net>
2015-03-04 23:22:14 +0100

    http://blog.wenzlaff.de/?p=1254

--- DS18B20/DS18B20.ino
+++ DS18B20/DS18B20.ino
@@ -1,77 +1,25 @@
 
 #include "OneWire.h"
+#include "DallasTemperature.h"
 
 // DS18S20 Temperature chip i/o
 OneWire ds(12);  // on pin 12
 
-void setup(void) {
-  // 1wire
-  // initialize inputs/outputs
+// decode temperature values
+DallasTemperature sensors(&ds);
+
+void setup(void)
+{
   // start serial port
-  Serial.begin(9600);
+  Serial.begin(11500);
+  sensors.begin();
 }
 
 void loop(void)
 {
-    //1 wire
-  byte i;
-  byte present = 0;
-  byte data[12];
-  byte addr[8];
+  sensors.requestTemperatures(); // Temp abfragen
 
-  ds.reset_search();
-  if ( !ds.search(addr)) {
-      Serial.print("No more addresses.\n");
-      ds.reset_search();
-      return;
-  }
-
-  Serial.print("R=");
-  for( i = 0; i < 8; i++) {
-    Serial.print(addr[i], HEX);
-    Serial.print(" ");
-  }
-
-  if ( OneWire::crc8( addr, 7) != addr[7]) {
-      Serial.print("CRC is not valid!\n");
-      return;
-  }
-
-  if ( addr[0] == 0x10) {
-      Serial.print("Device is a DS18S20 family device.\n");
-  }
-  else if ( addr[0] == 0x28) {
-      Serial.print("Device is a DS18B20 family device.\n");
-  }
-  else {
-      Serial.print("Device family is not recognized: 0x");
-      Serial.println(addr[0],HEX);
-      return;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44,1);         // start conversion, with parasite power on at the end
-
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("P=");
-  Serial.print(present,HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print( OneWire::crc8( data, 8), HEX);
-  Serial.println();
+  Serial.print(sensors.getTempCByIndex(0) );
+  Serial.print(" Grad Celsius\n");
 }
-
-
 
